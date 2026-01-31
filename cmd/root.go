79fcc9903a8d619986791cd562bb4d1a3a854808
Joe Scanlon <joseph.scanlon@oracle.com>
2026-01-31 08:32:30 -0500

    fix dupe menu

--- cmd/root.go
+++ cmd/root.go
@@ -9,28 +9,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "oci-collector",
 	Short: "A utility belt for OCI tenancy management",
-	Long: `This is designed to be a loose collection of tools to help manage and monitor your OCI tenancy.
-		
-usage: oci-collector [command] [flags]
-		
-Specify the action you want to take:
-
-limits: fetch limits in all YOUR regions
-compute: fetch compute active instances in all YOUR regions
-config: check config file and print basic info
-peeps: fetch user counts (-r to show users)
-policies: fetch policy counts (-run to show policies -verbose to show statements)
-groups: fetch group counts (-run to show groups )
-support: fetch support tickets (-list to show tickets)
-capacity: check capacity in all YOUR regions (-ocpus to specify ocpus -memory to specify memory -type to specify shape type)
-capability: what types of 'things' are available for a shape type (-type to specify shape type)
-children: dealing with child tenancies
-object: fetch object storage info
-billing: fetch billing info
-network: network related info
-search: search for resources created by a user
-schedule: fetch schedule
-`,
+    Long: `A loose collection of tools to help manage and monitor your OCI tenancy. Use the commands below to perform specific actions.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 	return cmd.Help()
 },
